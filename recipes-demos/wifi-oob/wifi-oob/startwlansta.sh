smzahraee/meta-psdkla

commit c900f48e7be879cb3b2f6eea748f31568a119d73

recipes-demo: wifi-oob: Add support for AM69 SK interface name
The wifi demo scripts are configured to support interface name wlp1s0
Update the wifi startup scripts to accomodate the interface name
of AM69 SK.

Signed-off-by: Dasnavis Sabiya <sabiya.d@ti.com>

recipes-demos/wifi-oob/wifi-oob/startwlansta.sh
@@ -17,7 +17,7 @@ print (config.get('$section','$param'));
 filename=/usr/share/intel9260/wificfg
 defapname=J7SK-AP
 
-interfaceState=$(ip a s | grep -nr "wlp1s0")
+interfaceState=$(ip a s | grep -nr "w*p1s0")
 
 if [ -z "$interfaceState" ]; then
  exit
@@ -46,6 +46,14 @@ if [ -z "$WIFINAME" ] ; then
 fi
 
 sleep 5
+
+### add WLAN interface, if not present
+if [ ! -d /sys/class/net/$WLAN ]
+then
+  echo "adding $WLAN interface"
+  iw phy `ls /sys/class/ieee80211/` interface add $WLAN type managed
+fi
+
 ifconfig wlp1s0 up
 
 sleep 1

recipes-demos/wifi-oob/wifi-oob/startwlanap.sh
@@ -17,7 +17,7 @@ print (config.get('$section','$param'));
 filename=/usr/share/intel9260/wificfg
 defapname=J7SK-AP
 
-interfaceState=$(ip a s | grep -nr "wlp1s0")
+interfaceState=$(ip a s | grep -nr "w*p1s0")
 
 if [ -z "$interfaceState" ]; then
  exit
@@ -35,11 +35,11 @@ cd /usr/share/intel9260
 ./load_wlcore.sh
 sleep 1
 
-a3=$(iw wlp1s0 info | grep "addr" | awk '{split($0,a,":"); print a[3]}')
+a3=$(iw w*p1s0 info | grep "addr" | awk '{split($0,a,":"); print a[3]}')
 
-a4=$(iw wlp1s0 info | grep "addr" | awk '{split($0,a,":"); print a[4]}')
+a4=$(iw w*p1s0 info | grep "addr" | awk '{split($0,a,":"); print a[4]}')
 
-a5=$(iw wlp1s0 info | grep "addr" | awk '{split($0,a,":"); print a[5]}')
+a5=$(iw w*p1s0 info | grep "addr" | awk '{split($0,a,":"); print a[5]}')
 
 apname="${defapname}_${a3}${a4}${a5}"
 

commit bfd1755033b0295b7ee106078afddd6c632f200b

recipes-demo: wifi-oob: Declare WLAN variable.
WLAN variable holds the wifi instance name.
Update the WLAN variable with default name.

Fixes:  c900f48e7be8 ("recipes-demo: wifi-oob: Add support for AM69 SK interface name")

Signed-off-by: Dasnavis Sabiya <sabiya.d@ti.com>

recipes-demos/wifi-oob/wifi-oob/startwlansta.sh
@@ -48,6 +48,7 @@ fi
 sleep 5
 
 ### add WLAN interface, if not present
+WLAN=wlp1s0
 if [ ! -d /sys/class/net/$WLAN ]
 then
   echo "adding $WLAN interface"