smzahraee/meta-psdkla

commit c900f48e7be879cb3b2f6eea748f31568a119d73

recipes-demo: wifi-oob: Add support for AM69 SK interface name
The wifi demo scripts are configured to support interface name wlp1s0
Update the wifi startup scripts to accomodate the interface name
of AM69 SK.

Signed-off-by: Dasnavis Sabiya <sabiya.d@ti.com>

recipes-demos/wifi-oob/wifi-oob/startwlanap.sh
@@ -17,7 +17,7 @@ print (config.get('$section','$param'));
 filename=/usr/share/intel9260/wificfg
 defapname=J7SK-AP
 
-interfaceState=$(ip a s | grep -nr "wlp1s0")
+interfaceState=$(ip a s | grep -nr "w*p1s0")
 
 if [ -z "$interfaceState" ]; then
  exit
@@ -35,11 +35,11 @@ cd /usr/share/intel9260
 ./load_wlcore.sh
 sleep 1
 
-a3=$(iw wlp1s0 info | grep "addr" | awk '{split($0,a,":"); print a[3]}')
+a3=$(iw w*p1s0 info | grep "addr" | awk '{split($0,a,":"); print a[3]}')
 
-a4=$(iw wlp1s0 info | grep "addr" | awk '{split($0,a,":"); print a[4]}')
+a4=$(iw w*p1s0 info | grep "addr" | awk '{split($0,a,":"); print a[4]}')
 
-a5=$(iw wlp1s0 info | grep "addr" | awk '{split($0,a,":"); print a[5]}')
+a5=$(iw w*p1s0 info | grep "addr" | awk '{split($0,a,":"); print a[5]}')
 
 apname="${defapname}_${a3}${a4}${a5}"
 

recipes-demos/wifi-oob/wifi-oob/startwlansta.sh
@@ -17,7 +17,7 @@ print (config.get('$section','$param'));
 filename=/usr/share/intel9260/wificfg
 defapname=J7SK-AP
 
-interfaceState=$(ip a s | grep -nr "wlp1s0")
+interfaceState=$(ip a s | grep -nr "w*p1s0")
 
 if [ -z "$interfaceState" ]; then
  exit
@@ -46,6 +46,14 @@ if [ -z "$WIFINAME" ] ; then
 fi
 
 sleep 5
+
+### add WLAN interface, if not present
+if [ ! -d /sys/class/net/$WLAN ]
+then
+  echo "adding $WLAN interface"
+  iw phy `ls /sys/class/ieee80211/` interface add $WLAN type managed
+fi
+
 ifconfig wlp1s0 up
 
 sleep 1

commit b722076c089ed209aa3e9bd58cbab327ccf73ce9

recipes-demo: wifi-oob: Fix for AP name creation.
AP name is created dynamically based on wlan module MAC ID.
Since wildcard characters are used in the wlan module name, creating
the AP name is failing.
Removed wildcard characters from the AP name creation logic.

Fixes:  c900f48e7be8 ("recipes-demo: wifi-oob: Add support for AM69 SK interface name")

Signed-off-by: Dasnavis Sabiya <sabiya.d@ti.com>

recipes-demos/wifi-oob/wifi-oob/startwlanap.sh
@@ -35,11 +35,13 @@ cd /usr/share/intel9260
 ./load_wlcore.sh
 sleep 1
 
-a3=$(iw w*p1s0 info | grep "addr" | awk '{split($0,a,":"); print a[3]}')
+WLAN=$(ifconfig -a | grep -i "wlP" | awk -F '[: ]' '{print $1}')
 
-a4=$(iw w*p1s0 info | grep "addr" | awk '{split($0,a,":"); print a[4]}')
+a3=$(iw $WLAN info | grep "addr" | awk '{split($0,a,":"); print a[3]}')
 
-a5=$(iw w*p1s0 info | grep "addr" | awk '{split($0,a,":"); print a[5]}')
+a4=$(iw $WLAN info | grep "addr" | awk '{split($0,a,":"); print a[4]}')
+
+a5=$(iw $WLAN info | grep "addr" | awk '{split($0,a,":"); print a[5]}')
 
 apname="${defapname}_${a3}${a4}${a5}"
 